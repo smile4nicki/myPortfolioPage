0270881b75e6779101cfd883bb5d404bd6e3d2f6
smile4nicki <smile4nicki@Nicholas-MacBook-Air.local>
2018-12-12 12:41:06 +0000

    Updates

---
+++ precache-manifest.452e92cf71def46302dae0347011dd3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de9e1bfbbf905edd53fa340b108defa5",
+    "url": "/static/media/Forest.de9e1bfb.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3aba96f071d63b37a05",
+    "url": "/static/js/main.a3aba96f.chunk.js"
+  },
+  {
+    "revision": "57f7224ab1e753138aeb",
+    "url": "/static/js/1.57f7224a.chunk.js"
+  },
+  {
+    "revision": "a3aba96f071d63b37a05",
+    "url": "/static/css/main.61f41ebc.chunk.css"
+  },
+  {
+    "revision": "ea78e17c01f4c9b5d8298dadd079b32d",
+    "url": "/index.html"
+  }
+];
